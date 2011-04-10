c94ac0e442b3be099dc5144f34eb76cdddafb993
Casey Link <unnamedrambler@gmail.com>
2011-04-10 19:59:50 -0400

    add the undocommands source. forget it in the last commit

---
+++ UndoCommands.cpp
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2010 Casey Link <unnamedrambler@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "UndoCommands.h"
+
+#include "ImageModel.h"
+
+#include <QDebug>
+
+EditPixelCommand::EditPixelCommand(int x, int y, QColor old_color, QColor new_color, ImageModel * model, QUndoCommand* parent)
+    : QUndoCommand(parent)
+    , mX(x)
+    , mY(y)
+    , mBefore(old_color)
+    , mAfter(new_color)
+    , mModel(model)
+{
+
+}
+
+void EditPixelCommand::redo()
+{
+    mModel->setData( mModel->index(mY, mX), mAfter, Qt::DisplayRole );
+}
+
+void EditPixelCommand::undo()
+{
+    mModel->setData( mModel->index(mY, mX), mBefore, Qt::DisplayRole );
+}
+
+bool EditPixelCommand::mergeWith(const QUndoCommand* command)
+{
+    const EditPixelCommand *editPixelCommand = static_cast<const EditPixelCommand *>(command);
+    return editPixelCommand->mAfter == mAfter && editPixelCommand->mBefore == mBefore;
+}
+
---
+++ UndoCommands.h
@@ -0,0 +1,40 @@
+/*
+    Copyright (C) 2010 Casey Link <unnamedrambler@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include <QUndoCommand>
+#include <QImage>
+#include <QColor>
+
+class ImageModel;
+
+class EditPixelCommand : public QUndoCommand
+{
+public:
+    enum { Id = 1 };
+    EditPixelCommand(int x, int y, QColor old_color, QColor new_color, ImageModel* model, QUndoCommand* parent = 0 );
+    void undo();
+    void redo();
+    bool mergeWith(const QUndoCommand *command);
+    int id() const { return Id; }
+private:
+    int mX, mY;
+    QColor mBefore,mAfter;
+    ImageModel *mModel;
+    
+};
