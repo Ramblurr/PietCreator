113f1633161cdc7961084df3d1a1acafae7154aa
Richel Bilderbeek <richel@richelbilderbeek.nl>
2016-07-11 20:43:50 +0200

    Only install older version of libpng

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,4 +5,3 @@ apt-get install libgif-dev
 #apt-get install libgd-dev # Travis wants me to be explicit
 apt-get install libgd2-xpm-dev 2.0.36~rc1~dfsg-6ubuntu2.2
 apt-get install libpng12-dev
-apt-get install libpng16-dev
