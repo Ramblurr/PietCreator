c1a0b61169ff12c392380e430a473d432b5f1541
Richel Bilderbeek <richel@richelbilderbeek.nl>
2016-07-11 20:37:18 +0200

    Install all dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-apt-get install libgd-dev
-apt-get install libgif-dev
+apt-get install cmake
+apt-get install libqt4-dev
+apt-get install libgif-dev 
+#apt-get install libgd-dev # Travis wants me to be explicit
+apt-get install libgd2-xpm-dev 2.0.36~rc1~dfsg-6ubuntu2.2
+apt-get install libpng12-dev
+apt-get install libpng16-dev
