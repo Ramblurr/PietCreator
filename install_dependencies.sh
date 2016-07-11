c0c6a4be0ca286e2291463182ca8b1cd0ede6c5f
Richel Bilderbeek <richel@richelbilderbeek.nl>
2016-07-11 20:51:18 +0200

    Install libgd-xpm-dev

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,5 +3,5 @@ apt-get install cmake
 apt-get install libqt4-dev
 apt-get install libgif-dev 
 #apt-get install libgd-dev # Travis wants me to be explicit
-apt-get install libgd2-xpm-dev 2.0.36~rc1~dfsg-6ubuntu2.2
+apt-get install libgd2-xpm-dev
 apt-get install libpng12-dev
